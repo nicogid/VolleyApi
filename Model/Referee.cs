7db96fdc54be0ed19f30db5f85bd2da5ba58767b
nicogid <nicolas.gidon@gmail.com>
2021-11-24 15:24:28 +0100

    init program

---
+++ Model/Referee.cs
@@ -0,0 +1,11 @@
+﻿namespace VolleyApi.Model
+{
+    public class Referee
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string Password { get; set; }
+        public string UserId { get; set; }
+
+    }
+}
